0a700b707a192cd5e68c4befa7e330dd92579502
Skorix <SkorixOleg@gmail.com>
2020-11-07 03:31:14 +0300

    071120200331

--- DivisibilityOfNumbers/Form1.vb
+++ DivisibilityOfNumbers/Form1.vb
@@ -9,19 +9,29 @@
         sum = 0
 
         Do While n1 <> 0
+
             cifra = n1 Mod 10
             sum = sum + cifra
             n1 = n1 \ 10
+
         Loop
 
         If n <> 0 Then
+
             If sum Mod 3 = 0 Then
+
                 Label2.Text = "Число " & n & " делится на 3"
+
             Else
+
                 Label2.Text = "Число " & n & " не делится на 3"
+
             End If
+
         Else
+
             Label2.Text = "Вы Ввели 0"
+
         End If
 
     End Sub
@@ -36,12 +46,19 @@
             ost = n Mod 100
 
             If ost Mod 4 = 0 Then
+
                 Label2.Text = "Число " & n & " делится на 4"
+
             Else
+
                 Label2.Text = "Число " & n & " не делится на 4"
+
             End If
+
         Else
+
             Label2.Text = "Вы ввели 0"
+
         End If
 
     End Sub
@@ -56,12 +73,19 @@
             ost = n Mod 10
 
             If ost = 0 Or ost = 5 Then
+
                 Label2.Text = "Число " & n & " делитсяна 5"
+
             Else
+
                 Label2.Text = "Число " & n & " не делится на 5"
+
             End If
+
         Else
+
             Label2.Text = "Выввели 0"
+
         End If
 
     End Sub
